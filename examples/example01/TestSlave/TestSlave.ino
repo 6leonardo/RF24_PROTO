d3173dea7bcfddb222166800c37975b900a46025
leonardo6 <astevani@gmail.com>
2019-10-31 23:49:07 +0100

    first test

--- examples/example01/TestSlave/TestSlave.ino
+++ examples/example01/TestSlave/TestSlave.ino
@@ -1,48 +1,68 @@
 // arduino uno
-#include "../../../proto.h"
-//#include "printf.h"
+#include "proto.h"
+#include "printf.h"
+//#undef asser(x)
+//#define assert(x) Serial.print(#x); Serial.println(x); //assert(x)
+RF24 radio(8, 9);
+Proto uno{radio, "Slave1", 10, 5, false};
 
-RF24 radio(8,9);
-Proto uno(radio,"Slave1",10,5,true);
+Device temperature{Input, AnalogFloat, 1, 0, "Temp1"};
+Device humidity{Input, AnalogFloat, 2, 0, "Hum1"};
+Device level{Input, AnalogInt16, 3, 0, "Level1"};
 
-Device temperature{Input,AnalogFloat,0,0,"Temp1"};
-Device humidity{Input,AnalogFloat,0,0,"Hum1"};
-Device level{Input,AnalogInt16,0,0,"Level1"};
-
-
-void setup() {
+void setup()
+{
 	Serial.begin(115200);
-  	printf_begin();
- 	radio.begin();
-   	radio.setAutoAck(false);
-   	radio.setChannel(112);
-    radio.setRetries(3,5); // delay, count
-  	//radio.setPALevel(RF24_PA_MIN);
-  	radio.setDataRate(RF24_250KBPS);
-  	uno.deviceIndex.addDevice(temperature);
-  	uno.deviceIndex.addDevice(humidity);
-  	uno.deviceIndex.addDevice(level);
-  	//uno.setNewSlave(newSlaveConnected);
-  	//uno.setNewDevice(newDeviceConnected);
-  	
+	printf_begin();
+	SPI.begin();
+	Serial.print("1.");
+	radio.begin();
+	radio.setAutoAck(false);
+	radio.setChannel(112);
+	Serial.print("isChipConnected: ");
+	Serial.println(radio.isChipConnected());
+	radio.setRetries(3, 5); // delay, count
+	radio.setPALevel(RF24_PA_MIN);
+	radio.setDataRate(RF24_250KBPS);
+	Serial.print("2.");
+	uno.deviceIndex.addDevice(temperature);
+	Serial.print("3.");
+	uno.deviceIndex.addDevice(humidity);
+	Serial.print("4.");
+	uno.deviceIndex.addDevice(level);
+	Serial.print("5.");
+	//uno.setNewSlave(newSlaveConnected);
+	//uno.setNewDevice(newDeviceConnected);
+	//radio.printDetails();
 	uno.setup();
+	radio.printDetails();
+	Serial.print("6.");
 }
 
-void loop() {
-    static unsigned long t=millis();
-
-	uno.execute();
-
-    if(millis()-t>60000) {
-    	float x=rand()*1000;
-    	Serial.print("temp "); Serial.println(x);
-    	temperature.setAnalogFloat(x);
-    	x=rand()*1000;
-    	Serial.print("humidity "); Serial.println(x);
-    	humidity.setAnalogFloat(x);
-    	int16_t l=rand()*256;
-    	level.setAnalogInt16(l);
-    	Serial.print("level "); Serial.println(l);
-    	t=millis();
-    }
+void loop()
+{
+	static unsigned long t = millis();
+	static bool first = true;
+	//Serial.print("7.");
+	//uno.execute();
+	uno.loop();
+	//Serial.println("8.");
+	if (millis() - t > 60000 || first)
+	{
+		Serial.println("set");
+		first = false;
+		float x = rand() * 1000;
+		Serial.print("temp ");
+		Serial.println(x);
+		temperature.setAnalogFloat(x);
+		x = rand() * 1000;
+		Serial.print("humidity ");
+		Serial.println(x);
+		humidity.setAnalogFloat(x);
+		int16_t l = rand() * 256;
+		Serial.print("level ");
+		Serial.println(l);
+		level.setAnalogInt16(l);
+		t = millis();
+	}
 }
