466311a6912b3a39c4ea98ce076f00d58290103f
leonardo6 <astevani@gmail.com>
2019-11-01 15:14:17 +0100

    debug version

--- examples/example01/TestMaster/TestMaster.ino
+++ examples/example01/TestMaster/TestMaster.ino
@@ -1,72 +1,94 @@
 //ESP32
-#include "../../../src/proto.h"
+#include "proto.h"
 
-RF24 radio(8,9);
-Proto Master(radio,"Master",10,5,true);
+RF24 radio(22, 21);
+Proto Master(radio, "Master", 10, 5, true);
 
-int sonda1=0;
-int sonda2=0;
+int sonda1 = 0;
+int sonda2 = 0;
 
-void newSlaveConnected(Slave& slave) {
-	if(slave.compareName("sonda 1"))
-		sonda1=slave.radioId;
-	if(slave.compareName("sonda 2"))
-		sonda2=slave.radioId;
+void newSlaveConnected(Slave &slave)
+{
+  debug(F("newslave "));
+  debugn(slave.name);
+  if (slave.compareName("Slave1"))
+    sonda1 = slave.radioId;
+  if (slave.compareName("Slave2"))
+    sonda2 = slave.radioId;
 }
 
-void newDeviceConnected(Device& device) {
-	
+void newDeviceConnected(Device &device)
+{
+  debug(F("newdevice "));
+  debugn(device.name);
 }
 
-
-void setup() {
-	Serial.begin(115200);
-  	printf_begin();
- 	radio.begin();
-   	radio.setAutoAck(false);
-   	radio.setChannel(112);
-    radio.setRetries(3,5); // delay, count
-  	//radio.setPALevel(RF24_PA_MIN);
-  	radio.setDataRate(RF24_250KBPS);
-  	Master.setNewSlave(newSlaveConnected);
-  	Master.setNewDevice(newDeviceConnected);
-	Master.setup();
+void setup()
+{
+  Serial.begin(115200);
+  printf_begin();
+  SPI.begin();
+  radio.begin();
+  radio.setAutoAck(false);
+  radio.setChannel(112);
+  Serial.print("isChipConnected: ");
+  Serial.println(radio.isChipConnected());
+  radio.setRetries(3, 5); // delay, count
+  //radio.setPALevel(RF24_PA_MIN);
+  radio.setDataRate(RF24_250KBPS);
+  Master.setNewSlave(newSlaveConnected);
+  Master.setNewDevice(newDeviceConnected);
+  Serial.print("2.");
+  radio.txDelay = 128;
+  Master.setup();
+  radio.printDetails();
+  Serial.print("2.");
 }
 
-void loop() {
-    static unsigned long t=millis();
+void loop()
+{
+  static unsigned long t = millis();
 
-	Master.execute();
+  //Master.execute();
+  Master.loop();
+  if (millis() - t > 60000)
+  {
 
-    if(millis()-t>60000) {
-
-    	// put your main code here, to run repeatedly:
-    	if(sonda1!=0) {
-    		Slave* slave=Master.slaveIndex.getSlave(sonda1);
-    		if(slave->status&IsActive) {
-                Device* temp=Master.deviceIndex.getDevice("temp",sonda1);
-                Serial.println(temp->getAnalogFloat());
-                Device* led=Master.deviceIndex.getDevice("led",sonda1);
-                led->setDigital(true);
-            }
-    	}
+    // put your main code here, to run repeatedly:
+    if (sonda1 != 0)
+    {
+      Slave *slave = Master.slaveIndex.getSlave(sonda1);
+      if (slave != NULL)
+        if (slave->status & IsActive)
+        {
+          Device *temp = Master.deviceIndex.getDevice("temp1", sonda1);
+          if (temp != NULL)
+            Serial.println(temp->getAnalogFloat());
+          //Device* led=Master.deviceIndex.getDevice("led",sonda1);
+          //led->setDigital(true);
+        }
     }
+  }
 
-    if(millis()-t>2*60000) {
+  if (millis() - t > 2 * 60000)
+  {
 
-    	// put your main code here, to run repeatedly:
-    	if(sonda1!=0) {
-    		Slave* slave=Master.slaveIndex.getSlave(sonda1);
-    		if(slave->status&IsActive) {
-                Device* temp=Master.deviceIndex.getDevice("temp",sonda1);
-                Serial.println(temp->getAnalogFloat());
+    // put your main code here, to run repeatedly:
+    if (sonda1 != 0)
+    {
+      Slave *slave = Master.slaveIndex.getSlave(sonda1);
+      if (slave != NULL)
+        if (slave->status & IsActive)
+        {
+          Device *temp = Master.deviceIndex.getDevice("temp1", sonda1);
+          if (temp != NULL)
+            Serial.println(temp->getAnalogFloat());
+          /*
                 Device* led=Master.deviceIndex.getDevice("led",sonda1);
                 led->setDigital(false);
-                t=millis();
-            }
-    	}
+                */
+          t = millis();
+        }
     }
-
-
-
+  }
 }
--- examples/example01/TestSlave/TestSlave.ino
+++ examples/example01/TestSlave/TestSlave.ino
@@ -12,57 +12,59 @@ Device level{Input, AnalogInt16, 3, 0, "Level1"};
 
 void setup()
 {
-	Serial.begin(115200);
-	printf_begin();
-	SPI.begin();
-	Serial.print("1.");
-	radio.begin();
-	radio.setAutoAck(false);
-	radio.setChannel(112);
-	Serial.print("isChipConnected: ");
-	Serial.println(radio.isChipConnected());
-	radio.setRetries(3, 5); // delay, count
-	radio.setPALevel(RF24_PA_MIN);
-	radio.setDataRate(RF24_250KBPS);
-	Serial.print("2.");
-	uno.deviceIndex.addDevice(temperature);
-	Serial.print("3.");
-	uno.deviceIndex.addDevice(humidity);
-	Serial.print("4.");
-	uno.deviceIndex.addDevice(level);
-	Serial.print("5.");
-	//uno.setNewSlave(newSlaveConnected);
-	//uno.setNewDevice(newDeviceConnected);
-	//radio.printDetails();
-	uno.setup();
-	radio.printDetails();
-	Serial.print("6.");
+  Serial.begin(115200);
+  printf_begin();
+  SPI.begin();
+  Serial.print("1.");
+  radio.begin();
+  radio.setAutoAck(false);
+  radio.setChannel(112);
+  Serial.print("isChipConnected: ");
+  Serial.println(radio.isChipConnected());
+  radio.setRetries(3, 5); // delay, count
+  radio.setPALevel(RF24_PA_MIN);
+  radio.setDataRate(RF24_250KBPS);
+  Serial.print("2.");
+  uno.deviceIndex.addDevice(temperature);
+  Serial.print("3.");
+  uno.deviceIndex.addDevice(humidity);
+  Serial.print("4.");
+  uno.deviceIndex.addDevice(level);
+  Serial.print("5.");
+  //uno.setNewSlave(newSlaveConnected);
+  //uno.setNewDevice(newDeviceConnected);
+  //radio.printDetails();
+  radio.txDelay=128;
+  uno.setup();
+  radio.printDetails();
+  Serial.print("6.");
 }
 
 void loop()
 {
-	static unsigned long t = millis();
-	static bool first = true;
-	//Serial.print("7.");
-	//uno.execute();
-	uno.loop();
-	//Serial.println("8.");
-	if (millis() - t > 60000 || first)
-	{
-		Serial.println("set");
-		first = false;
-		float x = rand() * 1000;
-		Serial.print("temp ");
-		Serial.println(x);
-		temperature.setAnalogFloat(x);
-		x = rand() * 1000;
-		Serial.print("humidity ");
-		Serial.println(x);
-		humidity.setAnalogFloat(x);
-		int16_t l = rand() * 256;
-		Serial.print("level ");
-		Serial.println(l);
-		level.setAnalogInt16(l);
-		t = millis();
-	}
+  static unsigned long t = millis();
+  static bool first = true;
+  //Serial.print("7.");
+  //uno.execute();
+  //Serial.println("8.");
+  if (millis() - t > 60000 || first)
+  {
+    Serial.println("set");
+    first = false;
+    float x = rand() * 1000;
+    Serial.print("temp ");
+    Serial.println(x);
+    temperature.setAnalogFloat(x);
+    x = rand() * 1000;
+    Serial.print("humidity ");
+    Serial.println(x);
+    humidity.setAnalogFloat(x);
+    int16_t l = rand() * 256;
+    Serial.print("level ");
+    Serial.println(l);
+    level.setAnalogInt16(l);
+    t = millis();
+  }
+  uno.loop();
+
 }
